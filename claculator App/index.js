705a4f753a6bc4f0d80450f0b465d03b41d9b797
Anuj kumar <807anuj@gmail.com>
2024-02-18 23:55:31 +0530

    frontend developer

---
+++ claculator App/index.js
@@ -0,0 +1,9 @@
+let advanced1=document.querySelector('.Advanced');
+advanced1.addEventListener('click', ()=>{
+    alert("Add new future as soon posible");
+});
+let Newdisplay= document.querySelector('.newdisplay').value.innerHTML;
+let string="";
+string= Newdisplay;
+console.log(string);
+
